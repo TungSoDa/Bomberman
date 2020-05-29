a6a272a74aa481a5fcfe37646cc8935b9e39975b
TungSoDa <tungdang1999@gmail.com>
2020-05-30 01:28:50 +0700

    third commit

---
+++ src/uet/oop/bomberman/Bomberman.java
@@ -0,0 +1,12 @@
+package uet.oop.bomberman;
+
+import uet.oop.bomberman.exceptions.BombermanException;
+import uet.oop.bomberman.gui.Frame;
+
+public class Bomberman
+{
+	public static void main(String[] args) throws BombermanException
+	{
+		Frame mainwindow = new Frame();
+	}
+}
